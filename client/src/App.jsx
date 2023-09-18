4ccb4569b63583def2743a21c5dde7fbbf973ac7
Janet <janet.sihapanya@gmail.com>
2023-09-18 18:20:18 -0500

    Changed Switch to Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
 import GlobalStyles from "./GlobalStyles";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components";
-import { inspectUser, duelUsers } from './services/userService'
+import { inspectUser, duelUsers } from './services/userService';
 import { useEffect } from "react";
 import Home from "./screens/Home";
 import Inspect from "./screens/Inspect";
 import Duel from "./screens/Duel";
 
 function App() {
-
- 
  // Hook that runs after React has updated the DOM
   useEffect(() => {
-    inspectUser()
+    inspectUser();
+    duelUsers();
   }, [])
 
   return (
     <Router>
       <GlobalStyles />
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/inspect" component={Inspect} />
-        <Route exact path="/duel" component={Duel} />
-      </Switch>
+      <Routes >
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/inspect" element={<Inspect/>} />
+        <Route path="/duel" element={<Duel/>} />
+      </Routes>
     </Router>
   );
 }
